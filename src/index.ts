88d6e5d79904bbdde6407280f3d0a0963aeab227
Diego Francisco Ferraz Dini <di-ferraz@hotmail.com>
2024-09-16 18:08:17 +0000

    Added user registration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import db from "./database";
-import DatabaseManager, { Category, DatabaseResponse, DatabaseTables, Priority, Status, Team, User } from "./databaseManager";
-import { DiffieHellmanGroup } from "crypto";
+import DatabaseManager, { DatabaseResponse, DatabaseTables } from "./databaseManager";
+import { createHash } from 'crypto';
 
 const dbManager: DatabaseManager = db();
 
@@ -10,32 +10,18 @@ const app = express();
 const port = 30000;
 
 app.use(express.json());
-const data = {
-  creationDate: Date.now(),
-  deadline: "Date",
-  name: "string",
-  description: "string",
-  category: 1,
-  priority: 1,
-  status: 1,
-  team: 1,
-  responsible: 1
-}
 
-const newCategory:Category = {name:"Teste"}
-const newPriority:Priority = {name:"Teste"}
-const newStatus:Status = {name:"Teste"}
-const newUser:User = {name:"Teste",email:"Teste",login:"teste",password:"teste"}
-const newTeam:Team = {name:"Teste", owner:1}
-
-console.log(dbManager.insert({table:DatabaseTables.CATEGORY, data:newCategory}))
-console.log(dbManager.insert({table:DatabaseTables.PRIORITY, data:newPriority}))
-console.log(dbManager.insert({table:DatabaseTables.STATUS, data: newStatus}))
-console.log(dbManager.insert({table:DatabaseTables.USER, data:newUser}))
-console.log(dbManager.insert({table:DatabaseTables.TEAM, data:newTeam}))
-
-console.log(dbManager.insert({table:DatabaseTables.TASK, data}))
-
+app.post("/user", (req, res) => {
+  const hash = createHash('sha256');
+  hash.update(req.body.password)
+  const newUser = {name:req.body.name,
+    email:req.body.email,
+    login:req.body.login,
+    password:hash.digest('hex')}
+  const dbResponse:DatabaseResponse = dbManager.insert({table:DatabaseTables.USER,data:newUser})
+  res.status(dbResponse.code)
+  res.send(dbResponse.message)
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
